843b1ba9a7f14202339c87bf4eb14c3997ba738d
THZ4 <152109120+THZ4@users.noreply.github.com>
2025-07-12 09:46:24 +0200

    Delete backend directory

--- backend/src/main/java/de/example/budgetracker/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package de.example.budgetracker.repository;
-
-import de.example.budgetracker.model.User;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findByUsername(String username);
-}
